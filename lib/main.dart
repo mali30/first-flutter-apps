f1b393d0aaffc45beb5d68a502b0fd174897d6a1
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-05-30 13:25:46 -0400

    refactored widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // Deleted everything and will write from scratch!
 import 'package:flutter/material.dart';
 
-import "./question.dart";
-import "./answer.dart";
+import "./quiz.dart";
+import "./result.dart";
 
 // main method to run teh app
 void main() => runApp(MyApp());
@@ -19,52 +19,44 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
 
+  final _myQuestionList = const [
+    {
+      "questionText": "What is your name?",
+      "answer": ["Larry", "Curly", "Moe"]
+    },
+    {
+      "questionText": "What is your age?",
+      "answer": ["21", "22", "25"]
+    },
+    {
+      "questionText": "What is your favorite food?",
+      "answer": ["pizza", "chicken", "chick-fil-a"]
+    },
+  ];
+
   // here is where we set the State for the index
   void _answerMyQuestion() {
     setState(() {
       _questionIndex += 1;
     });
-    print(_questionIndex);
   }
-
+  
   @override
 //  build method must be called
   Widget build(BuildContext context) {
     // list of objects
-    var _myQuestionList = [
-      {
-        "questionText": "What is your name?",
-        "answer": ["Larry", "Curly", "Moe"]
-      },
-      {
-        "questionText": "What is your age?",
-        "answer": ["21" , "22" , "25"]
-      },
-      {
-        "questionText": "What is your favorite food?",
-        "answer": ["pizza", "chicken", "chick-fil-a"]
-      },
-    ];
 
     // takes in "named" arguments
     // home -> core widget
     return MaterialApp(
       home: new Scaffold(
       appBar: AppBar(title: Text("My First App")),
-      body: Column(
-        children: [
-          // Custom Widgets
-          // will display the current question from our map
-          Question(
-            _myQuestionList[_questionIndex]["questionText"],
-          ),
-          // pulls values out of list adds into one list instead of list of list
-          ...(_myQuestionList[_questionIndex]["answer"] as List<String>)
-              .map((answer) {
-            return Answer(_answerMyQuestion, answer);
-          }).toList()
-        ],
-      ),
+      body: _questionIndex < _myQuestionList.length
+          ? Quiz(
+              answerQuestion: _answerMyQuestion,
+              questionIndex: _questionIndex,
+              questions: _myQuestionList)
+          : Result() ,
     ));
   }
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final Function answerQuestion;
+  final int questionIndex;
+
+  Quiz({@required this.answerQuestion, @required this.questions, @required this.questionIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // Custom Widgets
+        // will display the current question from our map
+        Question(
+          questions[questionIndex]["questionText"],
+        ),
+        // pulls values out of list adds into one list instead of list of list
+        ...(questions[questionIndex]["answer"] as List<String>).map((answer) {
+          return Answer(answerQuestion, answer);
+        }).toList()
+      ],
+    );
+  }
+}
